18d67cc31c0b425dd05b888d1aa7d72a31711921
Medhat Mohamed Deif <135258669+Medo20300@users.noreply.github.com>
2025-02-22 12:41:41 +0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,39 +1,7 @@
 let express = require('express');
 let app = express();
 
+// Log "Hello World" to the console
+console.log("Hello World");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
